69705094167680b64327116b602f91ff8835e843
yonggyu <yonggyu01@naver.com>
2024-02-22 18:21:24 +0900

    회원가입기능 추가중

--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -2,8 +2,77 @@ import '../css/Loginpage.css'
 function Loginpage(){
 return(
 <>
-<div className="loginview">
-    <h1>로그인페이지야</h1>
+
+<div className="hero min-h-calc bg-base-200">
+  <div className="hero-content flex-col">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">가입,로그인 </h1>
+      <p className="py-1">반갑습니다. </p>
+    </div>
+
+
+{/* 로그인창 */}
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered"  />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered"  />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-5">
+          <button className="btn btn-neutral mb-2" onClick={(e)=>{e.preventDefault()
+
+          }}>Sign in</button>
+          <button className="btn btn-primary mb-2" onClick={(e)=>{e.preventDefault()
+
+}}>Login</button>
+          <button className="btn  btn-warning " onClick={(e)=>{e.preventDefault()
+
+}}>Kakao Login</button>
+        </div>
+      </form>
+    </div>
+
+      {/* 회원가입창 */}
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">ID</span>
+          </label>
+          <input type="userid" placeholder="userid" className="input input-bordered" required  />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered"  required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-5">
+          <button className="btn btn-neutral mb-2" onClick={(e)=>{e.preventDefault()
+          }}>Sign in</button>
+        </div>
+      </form>
+    </div>
+
+
+
+
+
+  </div>
 </div>
 </>
 )
--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -1,40 +1,50 @@
 import { useState,useRef } from "react"
+import Navbar from '../components/Navbar'
 import Login from '../components/loginpage'
 import { animated ,useSpring } from '@react-spring/web'
 import Footerline from '../components/Footerline';  
 function Dicemain(){
 const [mymy,setmy]  =useState(false)
 const deg=useRef(45)
-const {transform, api} = useSpring(() => ({
-        transform : `perspective(2700px) rotateX(${deg.current}deg) rotateY(${deg.current}deg) scale3d(.3, .3, 0.3)`
-  }))
+const [transform, settrans] = useState(null)
+const roll = useRef()
+function rolling(){
+    roll.current.style=''
+}
   function clickwhat(value){
-    
-        if(value=='Home'){
-            settop(!top)
+    console.log(deg)
+    if(mymy===false){
+         roll.current.style=''
+    }else if(value==='Home'){
+            deg.current = 45
+            settrans(`perspective(2700px) rotateX(-30deg) rotateY(${deg.current}deg) scale3d(.3, .3, 0.3)`)
+        }else if(value==="Right"){
+            deg.current += 45
+            settrans(`perspective(2700px) rotateX(-30deg) rotateY(${deg.current}deg) scale3d(.3, .3, 0.3)`)
+        }else if(value==="Left"){
+            deg.current-=45
+            settrans(`perspective(2700px) rotateX(-30deg) rotateY(${deg.current}deg) scale3d(.3, .3, 0.3)`)
+        }else if(value==="Open"){
+            roll.current.style=''
+            setmy(!mymy)
         }
     console.log(value)
   }
   const [top,settop] = useState(false)
-return(  <animated.div>
-   <button onClick={()=>{   
-        setmy(!mymy)
-    }}>눌러봐요</button>
-    
-   {/* <button onClick={handleClick}>왼족</button> */}
-
-    <div className={mymy? 'cube-wrapper mybest' : 'mybest' } style={top? transform : null} >
-    <div className={mymy? 'cube-front mybest' : 'mybest'}>
+return(  <animated.div className="bg-red-50">
+    <Navbar setmy={setmy} mymy={mymy} rolling={rolling}/>
+    <animated.div className={mymy? 'cube-wrapper mybest' : 'mybest' } ref={roll} style={{transform}} >
+    <div className={mymy? 'cube-front mybest' : 'mybest1'}>
     <Login></Login>
 
     </div>
    
-    <div className={mymy? "cube-back mybest" : 'mybest'}></div>
-    <div className={mymy? "cube-top mybest" : 'mybest'}></div>
-    <div className={mymy? "cube-bottom mybest" : 'mybest'}></div>
-    <div className={mymy? "cube-left mybest" : 'mybest'}></div>
-    <div className={mymy? "cube-right mybest" : 'mybest'}></div>
-</div>
+    <div className={mymy? "cube-back mybest" : 'mybest2'}></div>
+    <div className={mymy? "cube-top mybest" : 'mybest1'}></div>
+    <div className={mymy? "cube-bottom mybest" : 'mybest2'}></div>
+    <div className={mymy? "cube-left mybest" : 'mybest1'}></div>
+    <div className={mymy? "cube-right mybest" : 'mybest2'}></div>
+</animated.div>
 
 <Footerline clickwhat={clickwhat}/>
 </animated.div>
